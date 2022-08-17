54af23d46d0eff62122cff10ab0f99d3c6be5456
Sergey <kepkaenterprice@gmail.com>
2022-08-17 22:07:21 +0500

    Eloquent ORM part 1

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CountriesTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
